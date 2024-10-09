d022deaf40b8a3ede533705d9a60829b9327fda8
jheval <2113110046@untels.edu.pe>
2024-10-09 15:20:01 -0500

    uc2 textviews y botones

--- app/src/main/java/com/example/dam_jimmyhuerta/MainActivity.java
+++ app/src/main/java/com/example/dam_jimmyhuerta/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.dam_jimmyhuerta;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        // Encuentra el botón con el ID 'button_primero' y lo asigna a la variable 'btnl'
+        Button btnl = findViewById(R.id.button_enviar);
+        // Establece un listener para 'btnl' que muestra un mensaje de Toast cuando se hace clic
+        btnl.setOnClickListener(View -> Toast.makeText(this, "Se envió sus datos", Toast.LENGTH_SHORT).show());
     }
 }
